6cf9dcdfac26ccdd94c1d3b4c3879c5f2586df8e
Oddmund Strømme <oddmunds@iterate.no>
2019-11-22 22:40:00 +0100

    altså

--- src/Main.elm
+++ src/Main.elm
@@ -17,7 +17,9 @@ main =
 
 
 type alias Model =
-    { t : Float, delta : Float }
+    { t : Float
+    , delta : Float
+    }
 
 
 init : flags -> ( Model, Cmd Msg )
@@ -50,15 +52,15 @@ view model =
         , style "font-size" "13px"
         ]
         [ div [] [ text (String.fromFloat model.delta) ]
-        , div []
+        , Html.pre []
             [ text
                 ((List.range 0 70
                     |> List.map
                         (\y ->
-                            List.range 0 280 |> List.map (\x -> oneCell model.t x y) |> String.join ""
+                            List.range 0 150 |> List.map (\x -> oneCell model.t x y) |> String.join ""
                         )
                  )
-                    |> String.join " "
+                    |> String.join "\n"
                 )
             ]
         ]
@@ -66,53 +68,218 @@ view model =
 
 oneCell : Float -> Int -> Int -> String
 oneCell t x y =
-    xyToF t x y |> floatToCell
-
-
-xyToF : Float -> Int -> Int -> Int
-xyToF t x y =
     let
-        tt =
-            floor t
+        calc =
+            calc2
+
+        render =
+            render3
+    in
+    calc t x y |> render
+
+
+calc2 : Float -> Int -> Int -> Float
+calc2 t x y =
+    let
+        xf =
+            toFloat x
+
+        yf =
+            toFloat y
+
+        xx =
+            cos (fMod 360 (xf / (8 + fMod 1000 (t / 1000)) + t / 1000))
 
         yy =
-            y
+            cos (fMod 360 (yf / (8 + fMod 100 (t / 100)) + t / 1000))
     in
-    (tt // 99) * x * yy // 7
+    xx + yy
 
 
-floatToCell : Int -> String
-floatToCell f =
-    case modBy 10 f of
+calc1 : Float -> Int -> Int -> Float
+calc1 t x y =
+    let
+        xf =
+            toFloat x
+
+        yf =
+            toFloat y
+
+        xx =
+            cos (fMod 360 (xf / 8 + t / 1000))
+
+        yy =
+            cos (fMod 360 (yf / 8 + t / 1000))
+    in
+    xx + yy
+
+
+fMod : Int -> Float -> Float
+fMod i f =
+    let
+        deltpå =
+            f / toFloat i
+
+        gange =
+            f * deltpå
+    in
+    f - gange
+
+
+calc0 : Float -> Int -> Int -> Float
+calc0 t x y =
+    let
+        yy =
+            toFloat y
+
+        xx =
+            toFloat x
+    in
+    ((t / 99) * xx * yy / 7) / 360 |> fMod 1
+
+
+render3 : Float -> String
+render3 f =
+    let
+        a =
+            f * 6
+
+        n =
+            round a
+    in
+    case n of
         0 ->
-            "®"
-
-        1 ->
             "."
 
+        1 ->
+            ","
+
         2 ->
-            ":"
+            "v"
 
         3 ->
-            "+"
+            "V"
 
         4 ->
-            "o"
-
-        5 ->
-            "x"
-
-        6 ->
-            "O"
-
-        7 ->
-            "X"
-
-        8 ->
             "N"
 
-        9 ->
+        5 ->
             "M"
+
+        6 ->
+            "#"
+
+        _ ->
+            " "
+
+
+render2 : Float -> String
+render2 f =
+    let
+        a =
+            f * 8
+
+        n =
+            round a
+    in
+    case n of
+        0 ->
+            "▁"
+
+        1 ->
+            "▂"
+
+        2 ->
+            "▃"
+
+        3 ->
+            "▄"
+
+        4 ->
+            "▅"
+
+        5 ->
+            "▆"
+
+        6 ->
+            "▇"
+
+        7 ->
+            "█"
+
+        _ ->
+            " "
+
+
+render1 : Int -> String
+render1 f =
+    case modBy 10 f of
+        0 ->
+            "▁"
+
+        1 ->
+            "▂"
+
+        2 ->
+            "▃"
+
+        3 ->
+            "▄"
+
+        4 ->
+            "▅"
+
+        5 ->
+            "▆"
+
+        6 ->
+            "▇"
+
+        7 ->
+            "█"
+
+        8 ->
+            "░"
+
+        9 ->
+            "▒"
+
+        _ ->
+            " "
+
+
+render0 : Int -> String
+render0 f =
+    case modBy 10 f of
+        0 ->
+            ">>"
+
+        1 ->
+            "-."
+
+        2 ->
+            "-:"
+
+        3 ->
+            ".-"
+
+        4 ->
+            ".."
+
+        5 ->
+            ".:"
+
+        6 ->
+            ":-"
+
+        7 ->
+            ":."
+
+        8 ->
+            "::"
+
+        9 ->
+            "--"
 
         _ ->
             " "
